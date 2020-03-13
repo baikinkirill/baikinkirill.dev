3087dc2c3a7d5f62c6090c8e3bbc59f3839af7e4
FineBot <kirill.baikin@gmail.com>
2020-03-13 10:04:11 +0300

    A

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Jopa!</a>
+        Welcome to <a href="https://nextjs.org">Jopafff!</a>
       </h1>
 
       <p className="description">
