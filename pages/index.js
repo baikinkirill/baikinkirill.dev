3116f44e26e1a102a44a6bed1ecc531064f8896a
Kirill <kirill.baikin@gmail.com>
2022-09-07 23:04:31 +0300

    New website

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,19 @@
-import * as THREE from 'three';
-import React, { Suspense, useEffect, useState } from 'react';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Text } from '@react-three/drei';
-import { Depth, LayerMaterial, Noise } from 'lamina';
-import Noodles from '../components/Noodles';
+import React from 'react';
 import getSeo from '../services/getSeo';
+import styles from '../styles/index.module.scss';
 
 export default function Index() {
- const [height, setHeight] = useState(200);
-
- useEffect(() => {
-  setHeight(window.innerHeight);
-
-  window.addEventListener('resize', () => {
-   setHeight(window.innerHeight);
-  });
- }, []);
 
  return (
   <div>
    {getSeo('THIS IS WEBPAGE TITLE', 'THIS IS WEBPAGE DESCRIPTION')}
-   <Canvas dpr={[1, 2]} style={{ height: `${height}px` }} camera={{ position: [0, 0, 10], fov: 22 }}>
-    <Bg />
-    <Suspense fallback={null}>
-     <Noodles />
-     <Caption>{`FINEBOT.SITE\nСКОРО...\nКОГДА-НИБУДЬ...`}</Caption>
-     <Rig />
-    </Suspense>
-   </Canvas>
+   <div data-text='baikinkirill.dev' className={styles.glitch}>
+    <a href='https://github.com/baikinkirill'>baikinkirill.dev</a>
+   </div>
+   <span>Say no <a target={'_blank'} href='https://laptev.dev'>laptev.dev</a>!</span>
   </div>
  );
 }
-
-function Caption({ children }) {
- const { width } = useThree((state) => state.viewport);
- return (
-  <Text
-   position={[0, 0, -5]}
-   lineHeight={0.8}
-   font='/Ki-Medium.ttf'
-   fontSize={width / 8}
-   material-toneMapped={false}
-   anchorX='center'
-   anchorY='middle'>
-   {children}
-  </Text>
- );
-}
-
-function Rig({ v = new THREE.Vector3() }) {
- return useFrame((state) => {
-  state.camera.position.lerp(v.set(state.mouse.x / 2, state.mouse.y / 2, 10), 0.05);
- });
-}
-
-function Bg() {
- return (
-  <mesh scale={100}>
-   <boxGeometry args={[1, 1, 1]} />
-   <LayerMaterial side={THREE.BackSide}>
-    <Depth colorB='red' colorA='skyblue' alpha={1} mode='normal' near={130} far={200} origin={[100, 100, -100]} />
-    <Noise mapping='local' type='white' scale={1000} colorA='white' colorB='black' mode='subtract' alpha={0.2} />
-   </LayerMaterial>
-  </mesh>
- );
-}
-
 
 export function getStaticProps() {
  return {
--- services/getSeo.tsx
+++ services/getSeo.tsx
@@ -5,20 +5,20 @@ export default function getSeo(title: string, desc: string) {
   <Head>
    <meta
     name='keywords'
-    content='FineBot, файнбот, Баикин Кирилл, Baikin Kirill'
+    content='baikinkirill.dev, кириешка.рф, Баикин Кирилл, Baikin Kirill'
    />
    <meta name='description' content={desc} />
    <meta property='og:title' content={title} />
    <meta property='og:type' content='website' />
-   <meta property='og:url' content='https://finebot.site' />
+   <meta property='og:url' content='https://baikinkirill.dev' />
    <meta property='og:image' content='./images/cover.webp' />
    <meta name='og:description' content={desc} />
    <meta name='twitter:card' content='summary' />
    <meta name='twitter:description' content={desc} />
    <meta name='twitter:title' content={title} />
    <meta name='twitter:image' content='./images/cover.webp' />
-   <meta name='twitter:site' content='https://finebot.site' />
-   <link rel='canonical' href='https://finebot.site' />
+   <meta name='twitter:site' content='https://baikinkirill.dev' />
+   <link rel='canonical' href='https://baikinkirill.dev' />
    <link rel='apple-touch-icon' href='images/logo.png' />
    <meta name='mobile-web-app-capable' content='yes' />
    <meta name='apple-mobile-web-app-capable' content='yes' />
