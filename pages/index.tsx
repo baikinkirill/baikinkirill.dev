8c02d6a644a02a67761d416c239dc107fea99a00
Kirill <kirill.baikin@gmail.com>
2022-10-28 19:38:50 +0300

    Some fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import getSeo from '../services/getSeo';
-import styles from '../styles/index.module.scss';
-import { MainPage } from '../types/types';
-import { NextPageContext } from 'next';
-import Image from 'next/image';
+import React from 'react'
+import getSeo from '../services/getSeo'
+import styles from '../styles/index.module.scss'
+import { MainPage } from '../types/types'
+import { NextPageContext } from 'next'
+import Image from 'next/image'
 
-export default function Index(props: MainPage) {
- const { isVPN } = props;
- return (
+export default function Index (props: MainPage): React.ReactNode {
+  const { isVPN } = props
+  return (
   <div className={styles.parent}>
    {getSeo('Hi there', '')}
    <div data-text='baikinkirill.dev' className={styles.glitch}>
@@ -20,22 +20,19 @@ export default function Index(props: MainPage) {
     </div>
    )}
   </div>
- );
+  )
 }
 
-export function getServerSideProps({ req }: NextPageContext): { props: MainPage } {
- if (!req) {
+export function getServerSideProps ({ req }: NextPageContext): { props: MainPage } {
+  let isVPN = false
+  if (req != null) {
+    const ip = req.headers['x-forwarded-for'] ?? req.connection.remoteAddress
+    isVPN = ip?.includes(process?.env?.NEXT_APP_VPN_HOST ??
+   'something_long_string') ?? false
+  }
   return {
-   props: {
-    isVPN: false,
-   },
-  };
- }
-
- const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || '';
- return {
-  props: {
-   isVPN: ip.includes(process?.env?.NEXT_APP_VPN_HOST || '') || ip.includes('127.0.0.1'),
-  },
- };
+    props: {
+      isVPN
+    }
+  }
 }
