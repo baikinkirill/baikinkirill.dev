4e9f1d9aa3b5d1885f02796698c22be1462f4b86
Kirill <kirill.baikin@gmail.com>
2023-12-02 23:28:46 +0300

    feat: image fixes

--- src/shared/utils/metadata/metadata.constants.ts
+++ src/shared/utils/metadata/metadata.constants.ts
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import { KEYWORDS, SITE_NAME, SITE_URL } from '@/shared/constants';
 
 const images = [{
-    url: './images/cover.png',
+    url: '/images/cover.png',
     alt: SITE_NAME,
     width: 1280,
     height: 640
@@ -10,6 +10,7 @@ const images = [{
 
 export const defaultMetadata: Metadata = {
     title: 'Hi there!',
+    metadataBase: new URL(SITE_URL),
     keywords: KEYWORDS,
     description: null,
     openGraph: {
@@ -34,9 +35,9 @@ export const defaultMetadata: Metadata = {
         'msapplication-TileColor': '#da532c',
         'theme-color': '#ffffff',
         canonical: SITE_URL,
-        'apple-touch-icon': './images/apple-touch-icon.png',
-        'favicon-32x32': './images/favicon-32x32.png',
-        'favicon-16x16': './images/favicon-16x16.png',
-        'site.webmanifest': './site.webmanifest'
+        'apple-touch-icon': '/images/apple-touch-icon.png',
+        'favicon-32x32': '/images/favicon-32x32.png',
+        'favicon-16x16': '/images/favicon-16x16.png',
+        'site.webmanifest': '/site.webmanifest'
     }
 };
