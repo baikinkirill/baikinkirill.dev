f38ea24c4709196249ce6ff2a1ad47955da4afcf
Kirill <kirill.baikin@gmail.com>
2024-10-02 22:04:23 +0300

    chore: renamed twitter to x

--- src/app/_index/social/Social.tsx
+++ src/app/_index/social/Social.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
-import { GITHUB_URL, INSTAGRAM_URL, SHOW_SOCIAL, TELEGRAM_URL, TWITTER_URL } from '@/shared/constants';
+import { GITHUB_URL, INSTAGRAM_URL, SHOW_SOCIAL, TELEGRAM_URL, X_URL } from '@/shared/constants';
 import { EmailLink } from './EmailLink/EmailLink';
 
 import styles from './Social.module.scss';
@@ -14,7 +14,7 @@ export const SocialIcons: React.FC = () => {
         <section className={styles.social}>
             {Boolean(TELEGRAM_URL) && <SocialIcon url={TELEGRAM_URL} bgColor={'transparent'} network={'telegram'}/>}
             {Boolean(GITHUB_URL) && <SocialIcon url={GITHUB_URL} bgColor={'transparent'}/>}
-            {Boolean(TWITTER_URL) && <SocialIcon url={TWITTER_URL} bgColor={'transparent'}/>}
+            {Boolean(X_URL) && <SocialIcon url={X_URL} bgColor={'transparent'}/>}
             {Boolean(INSTAGRAM_URL) && <SocialIcon url={INSTAGRAM_URL} bgColor={'transparent'}/>}
             <EmailLink/>
         </section>
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -3,7 +3,7 @@ export const SITE_URL = String(process.env.SITE_URL);
 
 export const SHOW_SOCIAL = process.env.SHOW_SOCIAL;
 export const GITHUB_URL = process.env.GITHUB_URL;
-export const TWITTER_URL = process.env.TWITTER_URL;
+export const X_URL = process.env.X_URL;
 export const INSTAGRAM_URL = process.env.INSTAGRAM_URL;
 export const TELEGRAM_URL = process.env.TELEGRAM_URL;
 export const EMAIL_URL = process.env.EMAIL_URL;
